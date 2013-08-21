95a3067d7a8cc53def89f6395267b9786c540ee2
Daniel Pyrathon <pirosb3@gmail.com>
2013-08-21 17:05:43 +0200

    added positional routes

--- src/travel.js
+++ src/travel.js
@@ -1,3 +1,28 @@
+var ROUTE_RE = /({{\w+}})/g;
+
+var _parseRoute = function(route, args) {
+
+	// Parse route regex, if no matches, return the route
+	var matches = route.match(ROUTE_RE);
+	if (!matches) return route;
+
+	// Get first match and fetch property from args. if no
+	// matches, raise exception
+	var result = route;
+	matches.forEach(function(match) {
+		var property = match.slice(2, match.length -2);
+		var propertyArg = args[property];
+		if (!propertyArg){
+			throw new Error("Property " + property + " was not found.");
+		}
+
+		// Return new route with property
+		result = result.replace(match, propertyArg);
+	});
+
+	return result;
+}
+
 var _extend = function(oldParams, newParams) {
 	return $.extend({}, oldParams, newParams);
 }
--- tests.js
+++ tests.js
@@ -9,4 +9,17 @@ describe("ApiJS", function() {
 		var apiKey = Pearson.callEndpoint('/topten/{id}').getDefaultParams().apiKey;
 		expect(apiKey).toBe('myApiKey');
 	});
+	it("should be able to parse routes", function() {
+		var route = _parseRoute('/user/{{userId}}', { userId: 22 });
+		expect(route).toBe('/user/22');
+
+		var route = _parseRoute('/user/{{userId}}/book/{{bookName}}/', { userId: 22, bookName: 'Lorem' });
+		expect(route).toBe('/user/22/book/Lorem/');
+	});
+	it("should be able to get a url with positional args", function() {
+		var Pearson = createAPI({ apiKey: 'myApiKey' });
+
+		var call = Pearson.callEndpoint('/topten/{id}');
+		expect(call._buildURL()).toBe('/topten/{id}');
+	});
 });
